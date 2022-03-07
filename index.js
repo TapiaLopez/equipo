8164d3f8620880fdc34ba3f56311461b1f3b846a
Montse Tapia <tapia.lopez@bootcamp-upgrade.com>
2022-03-07 23:02:19 +0100

    Añadiendo código

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// 1- Crear una carpeta nueva en local con un fichero html que incluya un h1 con el texto “Hola Mundo” 
+// y un botón que ponga “Cambiar texto”
+// 2- Si están en parejas, agregar un fichero styles.css con los estilos que quieras
+// 3- Crear el repo local conectar con el repo remoto y hacer comit de esos cambios
+
+// El colaborador:
+// 1- Crear una rama “javascript”
+// 2- Crear un fichero js que cambie el texto del h1 a “Bye Mundo” al hacer click en el botón
+
+let i = 0;
